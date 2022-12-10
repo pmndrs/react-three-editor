3d442312b26ca21e9fcf22476e5532133cd8bd3e
Nikhil Saraf <nsaraf98@gmail.com>
2022-12-09 20:12:21 -0800

    examples cleanup

--- examples/r3f-journey-levels-level-1/vite.config.ts
+++ examples/r3f-journey-levels-level-1/vite.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from "vite"
 import { r3f } from "@react-three/editor/vite"
-import macros from "babel-plugin-macros"
+
 export default defineConfig({
-  plugins: [
-    r3f({
-      babelPlugins: [macros]
-    })
-  ]
+  plugins: [r3f()]
 })
