88c8277f96035b92ed73d04bc986772de35cdd36
Nikhil Saraf <nsaraf98@gmail.com>
2022-12-15 18:17:10 -0800

    expot useEditor

--- packages/editor/src/fiber/index.ts
+++ packages/editor/src/fiber/index.ts
@@ -1,5 +1,6 @@
 export { createPortal, extend, useLoader, useThree } from "@react-three/fiber"
 export { useControls } from "../editable/controls/useControls"
 export { editable, Editable } from "../editable/editable"
+export { EditorContext, useEditor } from "../editable/Editor"
 export { Canvas } from "./EditableCanvas"
 export { useFrame } from "./useFrame"
