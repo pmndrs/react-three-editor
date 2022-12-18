f35e4d59e4f0086ff4e7102558d348865408bb91
Dinesh Salunke <dineshsalunke@consultbop.com>
2022-12-18 10:48:01 +0530

    refactor: add strict mode for kitchen-sink example

--- examples/kitchen-sink/src/index.tsx
+++ examples/kitchen-sink/src/index.tsx
@@ -1,5 +1,10 @@
+import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { Application } from "./App"
 import "./styles.css"
 
-createRoot(document.getElementById("root")!).render(<Application />)
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Application />
+  </StrictMode>,
+)
