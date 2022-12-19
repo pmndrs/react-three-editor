302e38ed0a69d81e5abb5422dda33a9aeab61660
Dinesh Salunke <dineshsalunke@consultbop.com>
2022-12-20 00:13:00 +0530

    refactor: use pointer up event for click

--- packages/editor/src/editable/editable.tsx
+++ packages/editor/src/editable/editable.tsx
@@ -79,7 +79,7 @@ export function createEditable<K extends keyof JSX.IntrinsicElements, P = {}>(
       editableElement.forwardedRef = true
       const [mounted, setMounted] = useState(false)
 
-      const onPointerDown = useCallback(
+      const onPointerUp = useCallback(
         (e: any) => {
           props.onPointerDown?.(e)
           let id = editableElement.id
@@ -96,7 +96,7 @@ export function createEditable<K extends keyof JSX.IntrinsicElements, P = {}>(
             {...rest}
             {...editableElement.props}
             ref={mergeRefs([ref, forwardRef, (r) => setMounted(true)])}
-            onPointerDown={onPointerDown}
+            onPointerUp={onPointerUp}
           >
             {children}
           </Component>
