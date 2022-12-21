a7bfcc7de3574386ca28a4cac7f90721aff1a745
Dinesh Salunke <dineshsalunke@consultbop.com>
2022-12-22 00:37:01 +0530

    refactor: vite plugin export

--- packages/editor/src/vite/index.ts
+++ packages/editor/src/vite/index.ts
@@ -1,5 +1,18 @@
+import { PluginItem } from "@babel/core"
 import react from "@vitejs/plugin-react"
+import { PluginOption } from "vite"
+import { reactThreeEditorBabel } from "./babel"
+import { editor } from "./server"
 
-export const r3f = () => {
-  return [react()]
+export const r3f = ({
+  babelPlugins = []
+}: { babelPlugins?: PluginItem[] } = {}): PluginOption => {
+  return [
+    editor(),
+    react({
+      babel: {
+        plugins: [...babelPlugins, reactThreeEditorBabel]
+      }
+    })
+  ]
 }
