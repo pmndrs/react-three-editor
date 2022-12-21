f34aa65e5d552d2fdfebd9634e0ef70e9bc0d0b6
Dinesh Salunke <dineshsalunke@consultbop.com>
2022-12-22 01:30:41 +0530

    refactor: include the styles for command bar

--- packages/editor/src/vite/server/index.ts
+++ packages/editor/src/vite/server/index.ts
@@ -7,6 +7,18 @@ export const editor = (): Plugin => {
   return {
     name: "vite-plugin-react-three-editor",
     enforce: "pre",
+    transformIndexHtml: async (id, prop) => {
+      return [
+        {
+          tag: "link",
+          attrs: {
+            type: "text/css",
+            rel: "stylesheet",
+            href: "node_modules/@react-three/editor/assets/commandbar-styles.css"
+          }
+        }
+      ]
+    },
     configureServer: createConfigureServerMethod(options)
   }
 }
