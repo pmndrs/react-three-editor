64a23905ca975ebf8b06213091b98187dfade156
Nikhil Saraf <nsaraf98@gmail.com>
2022-12-23 00:07:52 -0800

    remove r3f-alpha character controller example

--- examples/character-controller/src/test-assets/simple-plane.tsx
+++
@@ -1,8 +0,0 @@
-export function SimplePlane() {
-  return (
-    <mesh position={[0, -20, 0]} rotation-x={-Math.PI * 0.5}>
-      <planeGeometry args={[5, 5]} />
-      <meshBasicMaterial />
-    </mesh>
-  );
-}
