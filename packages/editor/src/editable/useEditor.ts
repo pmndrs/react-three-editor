34489fc05e86e49008dfa24eddbc1b3e3e44e6ad
Dinesh Salunke <dineshsalunke@consultbop.com>
2022-12-24 00:42:31 +0530

    refactor: add type defs

--- packages/editor/src/editable/useEditor.ts
+++ packages/editor/src/editable/useEditor.ts
@@ -1,10 +1,10 @@
 import { useContext } from "react"
 import { Editor, EditorContext } from "./Editor"
 
-export const useEditor = (): Editor => {
+export const useEditor = <T extends Editor>(): T => {
   const editor = useContext(EditorContext)
   if (!editor) {
     throw new Error("useEditor must be used within a EditorProvider")
   }
-  return editor
+  return editor as T
 }
