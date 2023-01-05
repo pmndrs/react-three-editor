3b69e6403af03a716f9097a94809ae9a53c942be
Nikhil Saraf <nsaraf98@gmail.com>
2023-01-04 21:10:33 -0800

    reexport everything from r3f

--- packages/three/src/fiber/index.ts
+++ packages/three/src/fiber/index.ts
@@ -1,13 +1,29 @@
 export { Editable, editable } from "@editable-jsx/editable"
 export {
+  act,
+  addAfterEffect,
+  addEffect,
+  addTail,
+  advance,
   applyProps,
+  context,
   createEvents,
   createPortal,
   createRoot,
+  dispose,
+  events,
   extend,
   getRootState,
+  invalidate,
+  reconciler,
+  render,
+  unmountComponentAtNode,
+  useGraph,
+  useInstanceHandle,
   useLoader,
-  useThree
+  useStore,
+  useThree,
+  _roots
 } from "@react-three/fiber"
 export { ThreeEditorCanvas as Canvas } from "../ThreeEditorCanvas"
 export {
